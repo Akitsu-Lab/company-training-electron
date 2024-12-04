82fe57c9a4f3f269e38be9e870e352419f9958d4
長岡聡太 <>
2024-12-04 23:57:25 +0900

    tableをMUI仕様に変更

--- src/renderer/components/Employee/EmployeeList.tsx
+++ src/renderer/components/Employee/EmployeeList.tsx
@@ -1,27 +1,38 @@
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material';
 import { employees } from '../../data/employees';
 
 export default function EmployeeList() {
   return (
     <div>
       <h1>社員一覧</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>名前</th>
-            <th>年齢</th>
-          </tr>
-        </thead>
-        <tbody>
-          {employees.map((employee) => (
-            <tr>
-              <td>{employee.id}</td>
-              <td>{employee.name}</td>
-              <td>{employee.age}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <TableContainer component={Paper}>
+        <Table aria-label="社員リスト">
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>名前</TableCell>
+              <TableCell>年齢</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {employees.map((employee) => (
+              <TableRow>
+                <TableCell>{employee.id}</TableCell>
+                <TableCell>{employee.name}</TableCell>
+                <TableCell>{employee.age}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
